4802589e72defb218376b7cdb7be337902a4eea9
Zall <swordblaze234@gmail.com>
2024-11-14 21:11:56 +0700

    b4c5n

---
+++ main.js
@@ -0,0 +1,10 @@
+function clicks() {
+    $('#div1').hide();
+    $('#div2').show();
+    $('#div2').addClass("fadeinv2");
+        
+    var typed = new Typed('.katakata', {
+        strings: ["Cuma mau bilang", "Selamat Malam Bby ku", "Aku Sayang Banget Sama Kamu💑", "Semoga kamu ngga bosen bosen sama aku ya Bby", "I Love You Putri Nurfarhanie❤️"],
+        typeSpeed: 70
+    });
+}
