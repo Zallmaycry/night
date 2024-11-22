5bcb7f854646415c0505cb4d57ccd8e8398855a8
Zall <swordblaze234@gmail.com>
2024-11-22 14:00:07 +0700

    Update main.js

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ function clicks() {
     $('#div2').addClass("fadeinv2");
         
     var typed = new Typed('.katakata', {
-        strings: ["Cuma mau bilang", "Selamat Malam Bby ku", "Aku Sayang Banget Sama Kamu💑", "Semoga kamu ngga bosen bosen sama aku ya Bby", "I Love You Putri Nurfarhanie❤️"],
+        strings: ["Cuma mau bilang", "Selamat Siang", "Jangan Lupa makan nanti sakit", "Semoga Engga", "Makasih."],
         typeSpeed: 70
     });
 }
